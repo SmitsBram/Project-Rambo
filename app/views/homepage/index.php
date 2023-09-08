6461bcf5b9ab0c44977510c43609e432b53fb7f0
Shahed Amer <chanyeol6521354@gmail.com>
2023-09-08 17:35:37 +0200

    een basis html gemaakt

--- app/views/homepage/index.php
+++ app/views/homepage/index.php
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
-<h1>Homepage</h1>
-=======
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
->>>>>>> 28311bcf8b87629c223f9d2fb494beea98737d26
