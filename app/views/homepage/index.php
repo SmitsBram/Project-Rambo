f802939112710fb3d6adb9a2c98140ba2841a2ad
Shahed Amer <chanyeol6521354@gmail.com>
2023-09-08 13:01:52 +0200

    index aangemakt

--- app/views/homepage/index.php
+++ app/views/homepage/index.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once('../parts/header.php');
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-    Idk
-</body>
 
-</html>
+
+
+
+
+require_once('../parts/footer.php');
