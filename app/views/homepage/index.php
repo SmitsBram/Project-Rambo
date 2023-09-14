0dfbab98d06f6264b4096b14912ad046ce74cdd0
Shahed Amer <chanyeol6521354@gmail.com>
2023-09-14 14:14:00 +0200

    de positie van de header aangepast

--- app/views/homepage/index.php
+++ app/views/homepage/index.php
@@ -1,5 +1,7 @@
 <!-- <section class="banner">
     <h1>Verken Wereldse Smaken</h1>
-    <button><a href="#menu">Menu</a></button>
-    <button><a href="#reserveren">Reserveren</a></button>
+    <div class="btns">
+        <button><a href="#menu">Menu</a></button>
+        <button><a href="#reserveren">Reserveren</a></button>
+    </div>
 </section> -->
