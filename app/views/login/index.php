0753986579ab3db32fbde371c1c38e0ba6ddbad6
Bram <336796@student.mboutrecht.nl>
2023-09-21 13:55:53 +0200

    Login pagina scss

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Assets/scss/login.scss">
+    <link rel="stylesheet" href="../public\scss\login.scss"> 
     <title>Document</title>
 </head>
 <body>
     <div class="ga-terug-container">
-    <a href="homepage.html"> ←Ga terug</a>
+    <a href="../homepage/index.php">← Ga terug</a>
     </div>
 
     <div class="login-container">
