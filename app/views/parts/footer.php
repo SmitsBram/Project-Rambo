16d8d280f4c77405e3a211d0981b0818ad5cab71
omarutrecht <omarchebab@outlook.com>
2023-09-28 13:32:43 +0200

    een begin gemaakt aan de footer

--- app/views/parts/footer.php
+++ app/views/parts/footer.php
@@ -1,6 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/scss/footer.scss">
+    <title>Document</title>
+</head>
+<body>
 <footer>
-    
+<div class="footer">
+        <div class="footer-content">
+            <div class="footer-section">
+                <h3 class="brown-text">Adres</h3>
+                <p>Rocambolesque</p>
+                <p>Australiëlaan 12345 EC</p>
+                <p>Amsterdam, Nederland</p>
+            </div>
+            <div class="footer-section">
+                <h3 class="brown-text">Contact</h3>
+                <p>+31 06 123456789</p>
+                <p><a href="#">Neem Contact op →</a></p>
+            </div>
+            <div class="footer-section">
+                <h3 class="brown-text">Openingstijden</h3>
+                <p>Maandag - Donderdag</p>
+                <p>12:00 - 22:00</p>
+                <p>Vrijdag - Zaterdag</p>
+                <p>12:00 - 01:00</p>
+                <p>Zondag</p>
+                <p>Gesloten</p>
+            </div>
+        </div>
+    </div>
 </footer>
-
+    
 </body>
 </html>
