90aaba0500b741ecc7701312f95e8c68630daa28
Amine1Amine <334491@student.mboutrecht.nl>
2023-10-02 14:44:19 +0200

    Amine nieuwe containere aangemaakt voor de animaties en bars toegevoegd

--- app/views/menu/index.php
+++ app/views/menu/index.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/css/menu.css">
+    <link rel="shortcut icon" href="public/IMG/Logo/Rocambolesque-logo-DEF.png" type="image/x-icon">
     <title>Template</title>
 </head>
 <body>
-    <div class="Item1-wite-container" >
-        <h1 class="Item2-Titel">MENUKAART VOOR LUNCH EN DINER</h1>
-        <div class="Item3-chefs-bites-container"></div>
+<div class="Item1-wite-container">
+    <div class="item2-orange-bar"></div>
+    <h1 class="Item3-titel">MENUKAART VOOR LUNCH EN DINER</h1>
+    <div class="item4-animation-container">
+        <div class="item5-black-notch"></div>
+        <h1 class="item6-container-titel">CHEFS BITES</h1>
     </div>
+</div>
+
 </body>
 </html>
