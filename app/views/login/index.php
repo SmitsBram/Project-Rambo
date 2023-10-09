d67fdd4f20feadb0a75bf2972e6944b84290ba3b
Yulkaz <yulkazs@outlook.com>
2023-10-09 13:22:22 +0200

    Pagina titles veranderd voor betere namen.

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/css/login.css">
-    <title>Inloggen</title>
+    <title>Rocambolesque ® - Inloggen</title>
 </head>
 <body>
     <div class="ga-terug-container">
--- app/views/menukaart/index.php
+++ app/views/menukaart/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/header.css">
     <link rel="stylesheet" href="/public/scss/menukaart.scss">
-    <title>menukaart</title>
+    <title>Rocambolesque ® - Menu</title>
 </head>
 <body>
 
