a7fa11c8a0ad3d8554644f5a60f28fecfde13c5f
Bram <336796@student.mboutrecht.nl>
2023-10-12 12:25:31 +0200

    register(begin nog niet af)

--- app/controllers/Register.php
+++ app/controllers/Register.php
@@ -2,12 +2,31 @@
 
 class Register extends BaseController
 {
-    public function index()
-    {
-        $data = [
-            'title' =>  'Register'
-        ];
-    
-        $this->view('register/index', $data);
-    }
+public function __construct()
+{
+parent::__construct();
+}
+function index(){
+$this->view->render('register/index');
+}
+function signup(){
+$user_name=$_POST['user_name'];
+$email_id=$_POST['email_id'];
+$password=$_POST['password'];
+$count=$this->model->check_user($user_name,$email_id);
+if($count > 0){
+echo 'This User Already Exists';
+}
+else{
+$data = array(
+'id' =>null,
+'user_name' =>$_POST['user_name'],
+'email_id' =>$_POST['email_id'],
+'password' =>$_POST['password']
+);
+$this->model->insert_user($data);
+}
+header('location:index');
+}
+
 }
